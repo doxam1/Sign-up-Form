c71f444607068ed16ba182cad329f5277fe17d13
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-11-24 09:14:02 +0200

    added confirmation sign, fixed mobile height (svh)

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const confirmPass = document.querySelector('#confirmPassword');
 const submitBtn = document.querySelector('.signUpBtn');
 const errorMsg = document.querySelector('.passErrorMsg')
 
+
+confirmPass.addEventListener ('input', (e) => {
+    if (confirmPass.value == password.value) {
+        confirmPass.classList.remove('passNotConfirmed');
+    } else {
+        confirmPass.classList.add('passNotConfirmed');
+    }
+})
+
 submitBtn.addEventListener('click', (e) => {
     if (confirmPass.value != password.value) {
         e.preventDefault();
         errorMsg.style.color = 'red';
-        errorMsg.style.fontSize = '.6rem';
+        errorMsg.style.fontSize = '.8rem';
         errorMsg.textContent = '*passwords do not match'
         setTimeout(() => {
             errorMsg.textContent = '';
